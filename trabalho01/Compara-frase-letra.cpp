93371b6cb42def07d099d26f4400874332a3d9f3
vihsantos <vick_santos_vick@hotmail.com>
2019-12-28 20:56:47 -0300

    Trabalho

---
+++ trabalho01/Compara-frase-letra.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (){
+	
+	char frase[250], letra;
+	
+	printf("Insira uma frase\n");
+	gets(frase);
+	
+	printf("Insira uma frase\n");
+	letra = getchar();
+	
+	int x=0, cont=0;
+	
+	for (x=0; x<strlen(frase); x++){
+		if (frase[x]==letra)
+			cont++;
+	}
+	
+	printf("A letra %c aparece %d vezes", letra, cont);
+	
+	return 0;
+}
