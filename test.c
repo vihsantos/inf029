c0e00093489b69821cbdb6c6bd8a9819a3f1c786
Vitoria <vick_santos_vick@hotmail.com>
2020-03-20 20:47:00 -0300

    atualizações

--- test.c
+++ test.c
@@ -1,44 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- int insere_valor (int *p){
-	int x, cod, valor;
+int insere_um_valor (int *p, int valor){
+	int x;
 		for (x=0; x<10; x++){
-			if (p[x]==-1) {
-				printf("DIGITE 1 SE DESEJA ADCIONAR SOMENTE UM ELEMENTO NO VETOR.\n");
-				printf("DIGITE 2 SE DESEJA ADCIONAR UMA CADEIA DE ELEMENTOS NO VETOR.\n");
-				scanf("%d", &cod);
-				
-				switch(cod){
-					case 1: {
-						printf("INSIRA O VALOR \n");
-						scanf("%d", &valor);
-							p[x]= valor;
-						break;
-					}
-					case 2: {
-						
-						break;
-					}
-					default {
-						
-						break;
-					}
-				}
-				
+			if (p[x]== -1) {
+				p[x]=valor
+			}
+			else {
+				printf("ESTRUTURA CHEIA")
 			}
 		}
-	
 }
 
+int inicia_vet(int *ptvet){
+	int x;
+		for (x=0; x<10; x++){
+			ptvet[x]= -1;
+		}
+}
 
 int main (){
 	int vet[10], x, *vetp;
-	
-	for (x=0; x<10; x++){
-		vet[x]= -1;
-	}
 		*vetp= vet[10];
+		inicia_vet(*vetp);
 	
 	
 	return 0;
