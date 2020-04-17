256308f44a1a8291fe35ce93292691420c11c7ab
Vitoria <vick_santos_vick@hotmail.com>
2020-04-17 00:55:12 -0300

    Nova versão do trabalho dois em andamento

---
+++ TrabalhoDois1.0.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct estrutura_principal{
+	int vet[10];
+	int *vetprin;
+	int limite;
+	int pos;
+};
+
+struct estrutura_auxiliar {
+	int conteudo;
+	int tamanho;
+	int pos;
+	struct estrutura_auxiliar *novo;
+};
+
+int inserir_elemento_prin(){
+	
+}
+int inserir_elemento_aux (){
+	
+}
