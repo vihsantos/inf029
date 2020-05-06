4eceeb02a04a0360f749acbac57909630d18bd7e
Vitoria <vick_santos_vick@hotmail.com>
2020-05-05 21:06:05 -0300

    a

--- TrabalhoDois1.0.c
+++ TrabalhoDois1.0.c
@@ -4,20 +4,19 @@
 struct estrutura_principal{
 	int vet[10];
 	int *vetprin;
-	int limite;
 	int pos;
 };
 
 struct estrutura_auxiliar {
-	int conteudo;
-	int tamanho;
-	int pos;
+	int *conteudo;
+	int casinhas;
+	int contador;
 	struct estrutura_auxiliar *novo;
 };
 
 int inserir_elemento_prin(int p, int *vet, int valor){
-	if (pos<10){
-		vet[pos]=valor;
+	if (p<10){
+		vet[p]=valor;
 		return 0;
 	}
 	else
@@ -27,8 +26,16 @@ int inserir_elemento_prin(int p, int *vet, int valor){
 	
 }
 
-int inserir_elemento_aux (){
+void inserir_elemento_aux (int pos, int *conteud, int tam, int *vetor){
+	int x;
+	vetor[pos]= *conteud;
+	conteud= NULL;
 	
+	conteud=(int *) malloc(tam*sizeof(int));
+		for (x=0; x<=tam; x++){
+			printf("Insira um numero: ");
+			scanf("%d", &conteud[x]);
+		}
 }
 
 int main (){
